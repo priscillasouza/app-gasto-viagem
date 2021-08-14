b7860b1a035f06a23809f705b7912a74c31e211d
priscilla <priscillasilva-24@hotmail.com>
2021-08-13 21:43:12 -0300

    create logic

--- app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -3,20 +3,43 @@ package com.example.gastodeviagem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_calcular.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(p0: View?) {
-                Toast.makeText(baseContext, "cliquei ${edt_distancia.text.toString()}", Toast.LENGTH_SHORT).show()
-            }
-        })
+        buttonCalculate.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if(id == R.id.buttonCalculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        if (validationOK()) {
+            val distance = editDistance.text.toString().toFloat()
+            val price = editPrice.text.toString().toFloat()
+            val autonomy = editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+            textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        }
+
+    }
+
+    private fun validationOK() : Boolean {
+        return (editDistance.text.toString() != ""
+                && editPrice.text.toString() != ""
+                && editAutonomy.text.toString() != "")
+    }
+
 }
